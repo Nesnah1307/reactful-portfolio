72baa1e47476d137b89b2873de1e29796b8a92ca
MackH <96394025+Nesnah1307@users.noreply.github.com>
2022-06-18 15:39:18 -0600

    now have project links showing

--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -2,8 +2,30 @@ import React from 'react';
 
 
 function Projects() {
+    return (
+        <section id="projs">
+            <h2>My Projects</h2>
+            <div className="blueIT">
+                <a className="github" href="https://github.com/Nesnah1307/blue-it"></a>
+                <a className="deployed" href="https://blue-it-dinosaur.herokuapp.com/">BlueIT</a>
+            </div>
+            
+            <div className="README">
+                <a className="github" href="https://github.com/Nesnah1307/pro-readme-generator">Pro README Generator</a>
+            </div>
+            
+            <div className="killer">
+            <a className="github" href="https://github.com/Nesnah1307/time-killer-3000"></a>
+            <a className="deployed" href="https://nesnah1307.github.io/time-killer-3000/">Time Killer 3000</a>
+            </div>
 
- 
+            <div className="budget">
+                <a className="github" href="https://github.com/Nesnah1307/ghost-pepper-ranch"></a>
+                <a className="deployed" href="https://shrouded-garden-23181.herokuapp.com/">Budget Tracker</a>
+            </div>
+        </section>
+    )
+
 }
 
 export default Projects;
