c6775f347e7a82264ab77467ac4e8bbacb3d29fc
MackH <96394025+Nesnah1307@users.noreply.github.com>
2022-06-17 19:39:51 -0600

    issues with displaying projects

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import About from './components/About';
 import Nav from './components/Nav';
+import Projects from './components/Projects';
 
 
 function App() {
+  const [currentPage, setCurrentPage] = useState("About");
+  
+  const renderPage = () => {
+    if (currentPage === 'About') {
+      return <About />;
+    }
+    if (currentPage === 'Projects') {
+      return <Projects />;
+    }
+  };
 
   return (
     <div>
-       <Nav></Nav>
+       <Nav currentPage={currentPage} setCurrentPage={setCurrentPage} ></Nav>
       <main>
-        <About></About>
+        {renderPage()}
       </main>
     </div>
   );
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,13 +1,9 @@
 import React from 'react';
 
+
+
 function Nav(props) {
-  const {
-    categories = [],
-    setCurrentCategory,
-    contactSelected,
-    currentCategory,
-    setContactSelected,
-  } = props;
+  const { currentPage, setCurrentPage } = props;
 
   return (
     <header className="flex-row px-1">
@@ -18,38 +14,15 @@ function Nav(props) {
       </h2>
       <nav>
         <ul className="flex-row">
-          <li className="mx-2">
-            <a data-testid="about" href="#about" onClick={() => setContactSelected(false)}>
-              About me
-            </a>
+          <li className={currentPage === "about" ? "mx-2 navActive" : "mx-2"}>
+            <span onClick={() => setCurrentPage("about")}>About Me</span>
           </li>
-          <li className="mx-2">
-            <a data-testid="portfolio" href="#portfolio" onClick={() => setContactSelected(false)}>
-                Portfolio
-            </a>
+          <li className={currentPage === "projects" ? "mx-2 navActive" : "mx-2"}>
+            <span onClick={() => setCurrentPage("projects")}>Projects</span>
           </li>
-          <li className={`mx-2 ${contactSelected && 'navActive'}`}>
-            <span onClick={() => setContactSelected(true)}>Contact</span>
-          </li>
-          {categories.map((category) => (
-            <li
-              className={`mx-1 ${
-                currentCategory.name === category.name && !contactSelected && 'navActive'
-                }`}
-              key={category.name}
-            >
-              <span
-                onClick={() => {
-                  setCurrentCategory(category);
-                  setContactSelected(false);
-                }}
-              >
-              </span>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </header>
+      </ul>
+    </nav>
+    </header >
   );
 }
 
